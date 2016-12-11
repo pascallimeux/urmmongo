a7ded981f8f5b45738d529aec289ca6238ebd8fb
pascallimeux <pascal.limeux@orange.com>
2016-12-11 18:49:02 +0100

    modify builder script

--- server/build_urmmongo.sh
+++ server/build_urmmongo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Build urmmongo"
 go build -ldflags "-s" main.go
-mv server ../dist/urmmongo
+mv main ../dist/urmmongo
 cp config.json ../dist/config.json
 cp ../scripts/*.sh ../dist
